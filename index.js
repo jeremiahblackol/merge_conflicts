b17e7d5bd0f5239fe41b4ee5659c4f8d8f22a6b6
Heather Faerber <heather@turing.com>
2022-09-01 10:48:18 -0600

    Refactor to fix naming conventions

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-var array = ['Chase', 'Cap', 'Luna', 'Toph']
+var names = ['Chase', 'Cap', 'Luna', 'Toph']
 
-var catInfo = {
+var cat1 = {
   name: 'Pep',
   color: 'black',
   age: 4,
   traits: ['shy', 'nice']
 }
 
-function numbers(num1, num2) {
-var sum = num1 + num2
-return sum
+var cat2 = {
+  name: 'Baby',
+  color: 'black',
+  age: 2,
+  traits: ['shy', 'cuddly']
 }
 
-function dogGreeting(array) {
-for (var i = 0; i < array.length; i++) {
-var greeting = `Hi ${array[i]}!  Who is a good pup?!`
-console.log(greeting)
+function getSum(num1, num2) {
+  var sum = num1 + num2
+  return sum
 }
+
+function printDogGreeting(dogNames) {
+  for (var i = 0; i < dogNames.length; i++) {
+    var greeting = `Hi ${dogNames[i]}!  Who is a good pup?!`
+    console.log(greeting)
+  }
 }
 
 function getCatAge(catInfo) {
