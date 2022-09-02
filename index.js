4564b2cd53605d2f16e3fff4fddedf76dadf5882
Heather Faerber <heather@turing.com>
2022-09-02 10:53:46 -0600

    Invoke functions

--- index.js
+++ index.js
@@ -14,10 +14,23 @@ var cat2 = {
   traits: ['shy', 'cuddly']
 }
 
+var cat3 = {
+  name: 'Chula',
+  color: 'multi',
+  age: 6,
+  traits: ['confident', 'jungle cat', 'patient']
+}
+
 function getSum(num1, num2) {
   var sum = num1 + num2
   return sum
 }
+
+getSum(10, 2)
+  // should give me back 12
+
+getSum(100, 1)
+  // should give me back 101
 
 function printDogGreeting(dogNames) {
   for (var i = 0; i < dogNames.length; i++) {
@@ -26,7 +39,16 @@ function printDogGreeting(dogNames) {
   }
 }
 
+printDogGreeting(names)
+  //will console log once for each name in the names array found on line 1
+
 function getCatAge(catInfo) {
   var catAge = catInfo.age
   return catAge
 }
+
+getCatAge(cat2)
+  // should give me back 2 because the object called cat2 has an age of 2
+
+getCatAge(cat1)
+  // should give me back 4 because the object called cat1 has an age of 4
